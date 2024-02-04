d1db27d7a407ff5e7adb9f85244a63c0e5518011
Nomena4 <119030942+Nomena-1918@users.noreply.github.com>
2024-02-04 16:40:39 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
+import { Route } from 'react-router-dom';
+import { IonApp, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import './App.css';
 import Login from './components/Body/Login';
